02ef0b497285e1435b080e8b28406559e7033814
sarajit01 <sarajitbarmon3@gmail.com>
2017-11-30 19:08:54 +0600

    gg

--- metronic_v5.0/theme/dist/preview/inc/view/demos/demo2/layout/header/hor-menu.php
+++
@@ -1,9 +0,0 @@
-<!-- begin::Horizontal Menu -->
-<div class="m-stack__item m-stack__item--middle m-stack__item--fluid">
-	<div id="m_header_menu" class="m-header-menu m-aside-header-menu-mobile m-aside-header-menu-mobile--offcanvas <?php Page::printClasses('header_menu', false);?>" <?php Page::printAttrs('header_menu')?> >
-		<ul class="m-menu__nav <?php Page::printClasses('header_menu_nav', false)?>">
-			<?php Menu::renderHorMenu(Page::getOption('menu', 'header/items'), Page::getOption('menu', 'header/self'))?>
-		</ul>
-	</div>
-</div>
-<!-- end::Horizontal Menu -->
